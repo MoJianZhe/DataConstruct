5c3f1a39489277d9147e18e8c77f55e0ea912bcd
muren <2423356283@qq.com>
2016-09-24 01:32:32 +0800

    zuidagongyueshu

--- src/com/dyoon/homework/test/Chapter1130.java
+++ src/com/dyoon/homework/test/Chapter1130.java
@@ -9,20 +9,26 @@ package com.dyoon.homework.test;
 public class Chapter1130 {
 	//判断这两个数是不是互质
 	public boolean isRelativeLyPrime(int x,int y){
+		if(greateCommonDivisor(x, y)==1){//如果两个数的最大公约数为1，则他们是互质的
+			return true;
+		}else{
+			return true;
+		}
 		
-		return true;
 	}
 //	求两个数的最大公约数
 	public static int greateCommonDivisor(int x,int y){
 		if(x<y){
 			int temp=0;;
 			temp=x;x=y;y=temp;
-		}while((x%y)!=0){
-			
 		}
-		
-		
-		return 0;
+		while((x%y)!=0){
+			int c=x%y;//如果余数能被除数所除，那么他一定可以被被除数所除
+			x=y;y=c;
+		}
+		return y;
 	}
-	
+	public static void main(String[] args) {
+		System.out.println(greateCommonDivisor(3,11));
+	}
 }
