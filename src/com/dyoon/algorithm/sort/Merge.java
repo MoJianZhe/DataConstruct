157e50fee6970af83aa91e07f7dbfc7c2c78838e
muren <2423356283@qq.com>
2017-01-18 23:28:16 +0800

    自底向上排序

--- src/com/dyoon/algorithm/sort/Merge.java
+++ src/com/dyoon/algorithm/sort/Merge.java
@@ -1,12 +1,21 @@
 package com.dyoon.algorithm.sort;
 
 /**
+ * 默认两个数组有序，然后将它们归并
  * Created by Administrator on 2017/1/16.
  */
 public class Merge {
 
 
     //将两个有序的子数组归并为一个数组
+
+    /**
+     *
+     * @param a 排序的数组
+     * @param low 第一个数组的起始下标
+     * @param mid 中间位置
+     * @param hight 第二个数组的结束下标
+     */
     public static void merge(int[] a, int low, int mid, int hight) {
         int i=low;int j=mid+1;
         int[] aum = new int[hight+1];
@@ -17,14 +26,15 @@ public class Merge {
             if(i>mid) a[k] = aum[j++];
             else if(j>hight) a[k] = aum[i++];
             else if (aum[i] < aum[j]) {
-                a[k] = a[i++];
+                a[k] = aum[i++];
             } else {
-                a[k] = a[j++];
+                a[k] = aum[j++];
             }
 
         }
     }
 
+    //自顶向下
     public static void sort(int[] a, int low, int high) {
         if(low>=high) return;
         int mid=low+(high-low)/2;
@@ -42,7 +52,8 @@ public class Merge {
             System.out.println(b[i]);
 
         }
-        sort(b, 0, b.length -1);
+//        sort(b, 0, b.length -1);
+        sortFromBottom(b);
         System.out.println("排序后");
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
@@ -50,6 +61,17 @@ public class Merge {
         }
     }
 
+    //自底向上
+    public static void sortFromBottom(int[] a) {
+        int N=a.length;
+        for (int size = 1; size <N ; size=size+size) {//size:排序数组的长度,size从1开始
+            //数组的长度依次为：1,2,4......
+            for (int low = 0; low < N-size; low=low+size+size) {
+                merge(a,low,low+size-1,Math.min(low+size+size-1,N-1));//low+size+size下标可能比a的最大下标要大
+            }
 
+        }
+
+    }
 
 }
